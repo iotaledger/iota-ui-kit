3fc6f7dd9cfe76b5eac6913871323ad1033b7f7d
JCNoguera <jcnoguera@boxfish.studio>
2023-11-07 15:41:41 +0100

    chore: remove unnecessary comments

--- src/stories/icon/icon.stories.ts
+++ src/stories/icon/icon.stories.ts
@@ -3,7 +3,6 @@ import type { Meta, StoryObj } from '@storybook/svelte'
 import { default as IconComponent } from './icon.svelte'
 import { IconEnum, IconSize } from './icon.enums'
 
-// More on how to set up stories at: https://storybook.js.org/docs/svelte/writing-stories/introduction
 const meta = {
   title: 'Atoms/Icon',
   component: IconComponent,
@@ -22,9 +21,9 @@ const meta = {
 } satisfies Meta<IconComponent>
 
 export default meta
+
 type Story = StoryObj<typeof meta>
 
-// More on writing stories with args: https://storybook.js.org/docs/svelte/writing-stories/args
 export const Icon: Story = {
   args: {
     icon: IconEnum.UpRightArrow,
