f4a02193014fed183340ecdb73a3cc116f86965e
JCNoguera <jcnoguera@boxfish.studio>
2023-11-07 18:43:13 +0100

    feat: extend red colors and update destructive button colors

--- src/components/atoms/buttons/button.classes.ts
+++ src/components/atoms/buttons/button.classes.ts
@@ -16,16 +16,16 @@ export const VARIANT_COLORS: Record<
   },
   [ButtonType.Secondary]: {
     light:
-      'text-iota-gray-900 border-[1.5px] border-iota-blue-400 hover:bg-iota-blue-700/0.08 active:bg-iota-blue-700/d0.16 focus:outline focus:outline-2 focus:outline-iota-blue-500 focus:outline-offset-2',
-    dark: 'text-white border-[1.5px] border-iota-blue-400 hover:bg-white/0.08 active:bg-white/d0.16',
+      'text-iota-gray-900 border-[1.5px] border-iota-blue-400 hover:bg-iota-blue-700/0.08 active:bg-iota-blue-700/0.16',
+    dark: 'text-white border-[1.5px] border-iota-blue-400 hover:bg-white/0.08 active:bg-white/0.16',
   },
   [ButtonType.Ghost]: {
     light:
-      'text-iota-gray-900 hover:bg-iota-blue-700/0.08 active:bg-iota-blue-700/d0.16',
-    dark: 'text-white hover:bg-white/0.08 active:bg-white/d0.16',
+      'text-iota-gray-900 hover:bg-iota-blue-700/0.08 active:bg-iota-blue-700/0.16',
+    dark: 'text-white hover:bg-white/0.08 active:bg-white/0.16',
   },
   [ButtonType.Destructive]: {
     light:
-      'text-white bg-[#E73333] hover:bg-[#D81A1A] active:bg-[#B21A1A] focus:outline focus:outline-2 focus:outline-iota-blue-500 focus:outline-offset-2',
+      'text-white bg-iota-red-600 hover:bg-iota-red-700 active:bg-iota-red-800 focus:outline focus:outline-2 focus:outline-iota-blue-500 focus:outline-offset-2',
   },
 }
