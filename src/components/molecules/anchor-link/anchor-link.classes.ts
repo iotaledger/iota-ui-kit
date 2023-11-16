6a3b6ac1b88b0e9cf3dc38c75cc3c328ce6dc984
evavirseda <evirseda@boxfish.studio>
2023-11-16 16:21:32 +0100

    fix: switch  styles

--- src/components/molecules/anchor-link/anchor-link.classes.ts
+++ src/components/molecules/anchor-link/anchor-link.classes.ts
@@ -1,21 +1,21 @@
 import { Mode } from '$lib/enums'
 
 export const ICON_FILL = {
-  [Mode.Dark]: 'text-iota-blue-600',
-  [Mode.Light]: 'text-white',
+  [Mode.Light]: 'text-iota-blue-600',
+  [Mode.Dark]: 'text-white',
 }
 
 export const TEXT_COLORS = {
-  [Mode.Dark]: 'text-iota-gray-900',
-  [Mode.Light]: 'text-white',
+  [Mode.Light]: 'text-iota-gray-900',
+  [Mode.Dark]: 'text-white',
 }
 
 export const BACKGROUND_COLORS = {
-  [Mode.Dark]: 'hover:bg-iota-blue-700 hover:bg-opacity-0.08 active:bg-iota-blue-700 active:bg-opacity-0.16',
-  [Mode.Light]: 'hover:bg-white hover:bg-opacity-0.08 active:bg-white active:bg-opacity-0.16',
+  [Mode.Light]: 'hover:bg-iota-blue-700 hover:bg-opacity-0.08 active:bg-iota-blue-700 active:bg-opacity-0.16',
+  [Mode.Dark]: 'hover:bg-white hover:bg-opacity-0.08 active:bg-white active:bg-opacity-0.16',
 }
 
 export const BORDER_COLORS = {
-  [Mode.Dark]: 'border-black',
-  [Mode.Light]: 'border-white',
+  [Mode.Light]: 'border-black',
+  [Mode.Dark]: 'border-white',
 }
