378a3f6e8a0b70d5d1c6e541b6b511c788a7b68f
JCNoguera <jcnoguera@boxfish.studio>
2023-11-29 18:14:09 +0100

    feat: add Hero component to storybook

--- src/stories/organisms/hero.stories.svelte
+++ src/stories/organisms/hero.stories.svelte
@@ -1,10 +1,11 @@
-import type { Meta, StoryObj } from '@storybook/svelte'
+<script lang="ts" context="module">
+import type { Meta } from '@storybook/svelte'
 
 import { Hero as HeroComponent } from '$organisms'
 import { ButtonType, IconEnum } from '$components'
 import { HeroVariant } from '$components/organisms'
 
-const meta = {
+export const meta = {
     title: 'Organisms/Hero',
     component: HeroComponent,
     tags: ['autodocs'],
@@ -36,13 +37,22 @@ const meta = {
         },
     },
 } satisfies Meta<HeroComponent>
+</script>
 
-export default meta
+<script lang="ts">
+    import { isDarkModeEnabled } from '$lib/storybook'
+    import { Story, Template } from '@storybook/addon-svelte-csf'
+</script>
 
-type Story = StoryObj<typeof meta>
 
-export const Hero: Story = {
-    args: {
+<Template let:args>
+    {@const reactiveArgs = { ...args, darkmode: $isDarkModeEnabled }}
+    <HeroComponent {...reactiveArgs}></HeroComponent>
+</Template>
+
+<Story
+    name="Hero"
+    args={{
         backgroundVideo:
             'https://s3-figma-videos-production-sig.figma.com/video/624900379542150991/TEAM/5442/38b0/-4c06-431d-aef6-803d9cc1dceb?Expires=1701648000&Signature=bu3XS23KqkOlUieagh8dGh2gMGND9nBF~etZ8Xgwti1eIQDWLLrv1Yo3CC~I4t-6cs9It423ATmxcD2m1w11iFW5Do5vrgOfe8B0MSfZvoLA4iRVeNT2zRRvq20QpYccEx-NaIajEOFd8kVxRfkCYae4qJ9XBkQ719ycuqWp4OA0sltkQX8JdcnDIaMKwKRNYta2mWmQAmIZZgTVQVdJlxWG0MFFSCCkea4JUyP61-3wWmC6sIMn6P2631s2DzQ-pQWDBLX0flfXSgIDpD2XeeXcDaWg-Y831iPFa-ittLMlm4UuA4-sdUC50X385WghCDoLyHFEylfvNk-Th3y63w__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
         overline: 'Overline',
@@ -73,5 +83,5 @@ export const Hero: Story = {
         })),
         media: 'https://videos.ctfassets.net/xit7f234flxz/7BqsHwcJ0cjnSY77lhYyO7/5d5d9c6688a791d21cc651fa54f2896c/woman.webm',
         variant: HeroVariant.Secondary,
-    },
-}
+    }}
+/>
