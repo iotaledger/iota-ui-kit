de456a5bd77169008854b7a06f1b27c47e2a88b5
evavirseda <evirseda@boxfish.studio>
2023-12-14 17:17:24 +0100

    feat: add iconContent component

--- src/components/molecules/icon-content/icon-content.classes.ts
+++ src/components/molecules/icon-content/icon-content.classes.ts
@@ -1,11 +1,21 @@
-import { Mode } from '$lib/enums'
+import { Direction, Mode } from '$lib/enums'
 
 export const ICON_FILL = {
     [Mode.Light]: 'text-iota-blue-600',
     [Mode.Dark]: 'text-white',
 }
 
-export const TEXT_COLORS = {
+export const TITLE_COLORS = {
     [Mode.Light]: 'text-iota-gray-900',
     [Mode.Dark]: 'text-white',
+}
+
+export const DESCRIPTION_COLORS = {
+    [Mode.Light]: 'text-iota-gray-600',
+    [Mode.Dark]: 'text-white',
+}
+
+export const DIRECTION_CLASSES = {
+    [Direction.Row]: 'flex-row space-x-6',
+    [Direction.Column]: 'flex-col space-y-6',
 }
---
+++ src/stories/molecules/icon-content.stories.svelte
@@ -0,0 +1,51 @@
+<script lang="ts" context="module">
+    import { IconEnum } from '$components'
+    import { IconContent as IconContentComponent } from '$molecules'
+
+    export const meta = {
+        title: 'Molecules/IconContent',
+        component: IconContentComponent,
+        tags: ['autodocs'],
+        argTypes: {
+            icon: {
+                control: { type: 'select' },
+                options: Object.values(IconEnum),
+            },
+            darkmode: {
+                control: { type: 'boolean' },
+            },
+            title: {
+                control: { type: 'text' },
+            },
+            description: {
+                control: { type: 'text' },
+            },
+            direction: {
+                control: { type: 'select' },
+                options: Object.values(Direction),
+            },
+        },
+    } satisfies Meta<IconContentComponent>
+</script>
+
+<script lang="ts">
+    import { isDarkModeEnabled } from '$lib/storybook'
+    import { Story, Template } from '@storybook/addon-svelte-csf'
+    import type { Meta } from '@storybook/svelte'
+    import { Direction } from '$lib/enums'
+</script>
+
+<Template let:args>
+    {@const reactiveArgs = { ...args, darkmode: $isDarkModeEnabled }}
+    <IconContentComponent {...reactiveArgs}></IconContentComponent>
+</Template>
+
+<Story
+    name="IconContent"
+    args={{
+        icon: IconEnum.Bullet,
+        title: 'Content Title',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.',
+        darkmode: $isDarkModeEnabled,
+    }}
+/>
