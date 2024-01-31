55ba028cba6c288bbf7afb2181a31214aba32990
evavirseda <evirseda@boxfish.studio>
2024-01-31 20:07:43 +0100

    feat: add `BlogpostCard` component (#48)
    * feat: add blogPost card compoent
    
    * fix: update types
    
    * feat: remove unnecessary control
    
    ---------
    
    Co-authored-by: Begoña Alvarez <balvarez@boxfish.studio>

---
+++ src/components/molecules/blogpost-card/blogpost-card.classes.ts
@@ -0,0 +1,11 @@
+import { Mode } from '$lib/enums'
+
+export const TITLE_COLORS = {
+    [Mode.Light]: 'text-iota-gray-900',
+    [Mode.Dark]: 'text-white',
+}
+
+export const LABEL_COLORS = {
+    [Mode.Light]: 'text-iota-gray-600',
+    [Mode.Dark]: 'text-white',
+}
