c03e9f275f604853c9f5fb4de3c50bdf79de8106
JCNoguera <jcnoguera@boxfish.studio>
2024-02-29 21:23:22 +0100

    feat: use browser env instead of window

--- src/lib/utils/detect-device.util.ts
+++ src/lib/utils/detect-device.util.ts
@@ -1,9 +1,10 @@
+import { BROWSER } from 'esm-env'
+
 export function isMobileDevice() {
-    if (window !== undefined && window?.navigator) {
+    if (BROWSER) {
         const userAgent = window.navigator.userAgent
         const userAgentRegex = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i
         return userAgentRegex.test(userAgent)
     }
-
     return false
 }
