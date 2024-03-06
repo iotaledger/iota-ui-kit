bc9197ce9fe08c1d50711a67e4b1f8cd4cb7981c
JCNoguera <88061365+VmMad@users.noreply.github.com>
2024-03-06 09:45:37 +0100

    feat: correctly add sizes and tags for the `Title` component (#68)
    * feat: correctly add sizes and tags for the `Title` component
    
    * chore: add mobile classes
    
    * fix: improve subtitle reactive class

--- src/lib/components/atoms/title/title.classes.ts
+++ src/lib/components/atoms/title/title.classes.ts
@@ -1,12 +1,21 @@
 import { Mode, Position } from '$lib/enums'
 import { TitleSize } from './title.enums'
 
-export const TITLE_SIZES: Record<TitleSize, string> = {
-    [TitleSize.H2]: 'text-8xl leading-112',
-    [TitleSize.H3]: 'text-7xl leading-88',
-    [TitleSize.H4]: 'text-6xl leading-72',
-    [TitleSize.H5]: 'text-5xl leading-58',
-    [TitleSize.H6]: 'text-4xl leading-44',
+const TITLE_SIZES_START_POSITION: Record<TitleSize, string> = {
+    [TitleSize.Large]: 'text-6xl leading-72 sm:text-8xl sm:leading-112',
+    [TitleSize.Medium]: 'text-5xl leading-58 sm:text-7xl sm:leading-88',
+    [TitleSize.Small]: 'text-4xl leading-44 sm:text-6xl sm:leading-72',
+    [TitleSize.ExtraSmall]: 'text-3xl leading-9 sm:text-5xl sm:leading-58',
+}
+
+const TITLE_SIZES_CENTER_POSITION: Record<TitleSize, string> = {
+    ...TITLE_SIZES_START_POSITION,
+    [TitleSize.ExtraSmall]: 'text-3xl leading-9 xs:text-6xl xs:leading-72',
+}
+
+export const TITLE_SIZES_BY_POSITION: Record<Position, Record<TitleSize, string>> = {
+    [Position.Start]: TITLE_SIZES_START_POSITION,
+    [Position.Center]: TITLE_SIZES_CENTER_POSITION,
 }
 
 export const TITLE_POSITIONS: Record<Position, string> = {
--- src/lib/components/atoms/title/title.svelte
+++ src/lib/components/atoms/title/title.svelte
@@ -1,19 +1,27 @@
 <script lang="ts">
-    import { TitleSize } from './title.enums'
+    import { TitleSize, TitleTag } from './title.enums'
     import { Position } from '$lib/enums'
     import {
         OVERLINE_TEXT,
         SUBTITLE_TEXT,
         TITLE_POSITIONS,
-        TITLE_SIZES,
+        TITLE_SIZES_BY_POSITION,
         TITLE_TEXT_COLORS,
     } from './title.classes'
+    import { HEADING_TO_SIZE } from './title.constants'
+
+    /**
+     * The HTML tag to use for the title
+     * @type {TitleTag}
+     */
+    export let tag: TitleTag = TitleTag.H2
 
     /**
      * The size of the title
      * @type {TitleSize}
      */
-    export let size: TitleSize = TitleSize.H2
+    export let size: TitleSize | undefined = undefined
+
     /**
      * The overline text to display
      * @type {string}
@@ -36,22 +44,23 @@
      */
     export let title: string
 
-    $: sizeClass = TITLE_SIZES[size]
+    let fallbackSize: TitleSize = HEADING_TO_SIZE[tag]
+
+    $: fallbackSize = size || HEADING_TO_SIZE[tag]
+    $: sizeClass = TITLE_SIZES_BY_POSITION[position][fallbackSize]
     $: positionClass = TITLE_POSITIONS[position]
     $: titleColorClass = !darkmode ? TITLE_TEXT_COLORS.title.light : TITLE_TEXT_COLORS.title.dark
     $: textColorClass = !darkmode ? TITLE_TEXT_COLORS.text.light : TITLE_TEXT_COLORS.text.dark
-    $: subtitleTextClass = size === TitleSize.H5 ? SUBTITLE_TEXT.smaller : SUBTITLE_TEXT.default
-    $: spaceBetweenClass = size === TitleSize.H6 ? 'space-y-6' : 'space-y-12'
+    $: subtitleTextClass =
+        fallbackSize === TitleSize.ExtraSmall ? SUBTITLE_TEXT.smaller : SUBTITLE_TEXT.default
 </script>
 
-<div
-    class="flex flex-col space-y-6 lg:space-y-12 font-medium {positionClass} {spaceBetweenClass} layout--{position}"
->
+<div class="flex flex-col font-medium space-y-6 {positionClass} layout--{position}">
     {#if overline.length > 0}
         <span class="{OVERLINE_TEXT} {textColorClass}">{overline} </span>
     {/if}
 
-    <svelte:element this={size} class="{titleColorClass} {sizeClass}">
+    <svelte:element this={tag} class="{titleColorClass} {sizeClass}">
         {title}
     </svelte:element>
 
