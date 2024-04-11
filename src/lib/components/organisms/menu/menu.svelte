1af0819c400ae46d6e3e2679de593d49f78397bf
JCNoguera <jcnoguera@boxfish.studio>
2024-04-11 18:24:51 +0200

    feat: add mobile navbar

--- src/lib/components/organisms/menu/menu.svelte
+++ src/lib/components/organisms/menu/menu.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import { IconEnum, MenuItem, Icon, Logo } from '$components'
     import type { ComponentProps } from 'svelte'
-    import { CUSTOM_COLORS } from '$lib/constants'
 
     /**
      * Darkmode
@@ -21,30 +20,63 @@
      */
     export let activeIndex: number | undefined = undefined
 
+    /**
+     * Is mobile menu open
+     */
+    export let isMobileMenuOpen: boolean = false
+
     function getActiveIndexSetter(index: number): () => void {
         return () => (activeIndex = index)
     }
+
+    function toggleMobileMenu(): void {
+        isMobileMenuOpen = !isMobileMenuOpen
+    }
 </script>
 
-<nav class="flex flex-row items-center justify-between w-full px-6 lg:px-0">
-    <Logo {darkmode} />
-    <desktop-menu-items class="hidden lg:flex flex-row items-center space-x-4">
-        {#each menuItems as menuItem, index}
-            {@const handleOnClick = getActiveIndexSetter(index)}
-            <MenuItem
-                {...menuItem}
-                {darkmode}
-                isActive={activeIndex === index}
-                on:click={handleOnClick}
-            />
-        {/each}
-    </desktop-menu-items>
-    <mobile-menu class="flex lg:hidden">
-        <Icon
-            icon={IconEnum.Menu}
-            width={32}
-            height={32}
-            fill={darkmode ? 'white' : CUSTOM_COLORS['iota-gray'][900]}
-        />
-    </mobile-menu>
+{#if isMobileMenuOpen}
+    <button
+        class="fixed z-[-1] top-full left-0 outline-none border-none p-0 w-full h-screen cursor-auto"
+        on:click={toggleMobileMenu}
+    />
+{/if}
+
+<nav class="container w-full" class:relative={isMobileMenuOpen}>
+    <div class="flex flex-row items-center justify-between px-6 py-3 md:py-0">
+        <a href="/">
+            <Logo {darkmode} />
+        </a>
+        <desktop-menu-items class="hidden md:flex flex-row items-center space-x-4">
+            {#each menuItems as menuItem, index}
+                {@const handleOnClick = getActiveIndexSetter(index)}
+                <MenuItem
+                    {...menuItem}
+                    {darkmode}
+                    isActive={activeIndex === index}
+                    on:click={handleOnClick}
+                />
+            {/each}
+        </desktop-menu-items>
+        <button
+            on:click={toggleMobileMenu}
+            class="flex md:hidden {darkmode ? 'text-white' : 'text-iota-gray-900'}"
+        >
+            <Icon icon={IconEnum.Menu} width={32} height={32} currentColor />
+        </button>
+    </div>
+
+    <aside class="md:hidden w-full relative" class:hidden={!isMobileMenuOpen}>
+        <menu-items class="flex flex-col items-start justify-start space-y-8 py-[60px] px-6">
+            {#each menuItems as menuItem, index}
+                {@const handleOnClick = getActiveIndexSetter(index)}
+                <MenuItem
+                    {...menuItem}
+                    {darkmode}
+                    isActive={activeIndex === index}
+                    on:click={handleOnClick}
+                    mobile
+                />
+            {/each}
+        </menu-items>
+    </aside>
 </nav>
