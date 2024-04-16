52f80d023d00b0186bf85487b6185b8ae7cf7c5e
JCNoguera <jcnoguera@boxfish.studio>
2024-04-16 13:46:05 +0200

    styles: make TextSection break into column in screen md

--- src/lib/components/molecules/text-section/text-section.classes.ts
+++ src/lib/components/molecules/text-section/text-section.classes.ts
@@ -8,24 +8,24 @@ export const TEXT_COLORS: Record<Mode, string> = {
 }
 
 export const DIRECTION_CLASSES = {
-    [Direction.Row]: 'flex-col lg:flex-row justify-between space-y-6 lg:space-y-0 lg:space-x-28',
+    [Direction.Row]: 'flex-col md:flex-row justify-between space-y-6 md:space-y-0 md:space-x-28',
     [Direction.Column]: 'flex-col space-y-12 justify-start items-start text-left',
 }
 
 export const PADDING_TOP_WITH_OVERLINE_CLASSES: Partial<Record<TitleSize, string>> = {
-    [TitleSize.Large]: 'lg:pt-28',
-    [TitleSize.Medium]: 'lg:pt-[120px]',
-    [TitleSize.Small]: 'lg:pt-28',
+    [TitleSize.Large]: 'md:pt-28',
+    [TitleSize.Medium]: 'md:pt-[120px]',
+    [TitleSize.Small]: 'md:pt-28',
 }
 
 export const PADDING_TOP_WITHOUT_OVERLINE_CLASSES: Partial<Record<TitleSize, string>> = {
-    [TitleSize.Large]: 'lg:pt-16',
-    [TitleSize.Medium]: 'lg:pt-12',
-    [TitleSize.Small]: 'lg:pt-8',
+    [TitleSize.Large]: 'md:pt-16',
+    [TitleSize.Medium]: 'md:pt-12',
+    [TitleSize.Small]: 'md:pt-8',
 }
 
 export const PADDING_LEFT_CLASSES: Record<Direction, string> = {
-    [Direction.Row]: 'lg:pl-[104px]',
+    [Direction.Row]: 'md:pl-[104px]',
     [Direction.Column]: 'pl-0',
 }
 
