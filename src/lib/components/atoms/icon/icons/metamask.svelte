24dfc9f51cde502014149d9ae156365b76ce370c
JCNoguera <jcnoguera@boxfish.studio>
2024-04-19 13:17:37 +0200

    feat: add icons for interesting sites

---
+++ src/lib/components/atoms/icon/icons/metamask.svelte
@@ -0,0 +1,105 @@
+<script lang="ts">
+    export let width: number = 48
+    export let height: number = 48
+    export let currentColor: boolean = false
+    export let fill: string = currentColor ? 'currentColor' : 'white'
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" {width} {height} viewBox="0 0 48 48" fill="none">
+    <path
+        d="M23.971 35.0161H26.233L26.959 38.4311H23.624"
+        stroke={fill}
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+    <path
+        d="M42.158 31.771L30.5879 31.984L26.2329 35.016L26.6169 16.784L29.0079 10.85L40.621 6.23901L42.5 11.96L40.792 18.706L40.963 22.121L39.853 23.759L42.158 31.771ZM42.158 31.771L40.066 40.267L31.527 37.62L26.9589 41.761H23.9709"
+        stroke={fill}
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+    <path
+        d="M39.8529 23.7592L31.698 21.353"
+        stroke={fill}
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+    <path
+        d="M26.439 25.2438L34.3879 25.0674"
+        stroke={fill}
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+    <path
+        d="M35.6469 31.891L31.5269 37.62L30.5879 31.984L34.3879 25.067L31.6979 21.353L26.6169 16.784L40.621 6.23901"
+        stroke={fill}
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+    <path
+        d="M26.2329 35.0156L31.5272 37.62"
+        stroke={fill}
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+    <path
+        d="M27.45 29.663L30.636 28.478L28.362 27.293L27.45 29.663Z"
+        stroke={fill}
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+    <path
+        d="M29.008 10.8496H23.9707"
+        stroke={fill}
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+    <path
+        d="M24.029 35.0161H21.767L21.041 38.4311H23.624"
+        stroke={fill}
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+    <path
+        d="M5.842 31.771L17.412 31.984L21.767 35.016L21.383 16.784L18.992 10.85L7.379 6.23901L5.5 11.96L7.208 18.706L7.037 22.121L8.147 23.759L5.842 31.771ZM5.842 31.771L7.934 40.267L16.473 37.62L21.041 41.761H24.029"
+        stroke={fill}
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+    <path
+        d="M8.14722 23.7592L16.3021 21.353"
+        stroke={fill}
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+    <path
+        d="M21.5612 25.2438L13.6123 25.0674"
+        stroke={fill}
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+    <path
+        d="M12.3529 31.891L16.4729 37.62L17.4119 31.984L13.6119 25.067L16.3019 21.353L21.3829 16.784L7.37891 6.23901"
+        stroke={fill}
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+    <path
+        d="M21.7672 35.0156L16.4729 37.62"
+        stroke={fill}
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+    <path
+        d="M20.55 29.663L17.364 28.478L19.638 27.293L20.55 29.663Z"
+        stroke={fill}
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+    <path
+        d="M18.9919 10.8496H24.0292"
+        stroke={fill}
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+</svg>
