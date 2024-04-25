c4995bbde3e5f0a5d5bb8548430b36be2509bb16
JCNoguera <jcnoguera@boxfish.studio>
2024-04-25 15:07:36 +0200

    chore: revert app.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,6 @@
 <script lang="ts">
-    import { HighlightCardVariant, HighlightCard, IconEnum, MediaType } from '$components'
-    import type { ComponentProps } from 'svelte'
-
-    const props: ComponentProps<HighlightCard> = {
-        title: 'Send IOTA Tokens with Javascript',
-        overline: 'Overline',
-        subtitle: 'Subtitle',
-        icon: IconEnum.Atom,
-        variant: HighlightCardVariant.Hover,
-        backgroundMedia: {
-            src: 'https://files.iota.org/media/ui-kit/cards/black/compressed/IOTA_card_black_6.mp4',
-            type: MediaType.Video,
-        },
-        description:
-            'Allowing the EVM chain to communicate outside of the chain and interact, through the IOTA/Shimmer layer, with other chains and even non-EVM chain smart contracts',
-    }
 </script>
 
 <main>
-    <section class="container">
-        <HighlightCard {...props} />
-    </section>
+    <h1>Vite + Svelte + TS</h1>
 </main>
