eef421ce9a660a24fc55e8cf2e2ceaaed9872dc1
JCNoguera <jcnoguera@boxfish.studio>
2024-04-25 15:05:43 +0200

    chore: remove unused component

--- src/lib/components/molecules/highlight-card/index.ts
+++ src/lib/components/molecules/highlight-card/index.ts
@@ -1,4 +1,3 @@
 export { default as HighlightCard } from './highlight-card.svelte'
-export { default as HighlightCardSvelteTransition } from './highlight-card-svelte.svelte'
 
 export { HighlightCardVariant } from './highlight-card.enums'
