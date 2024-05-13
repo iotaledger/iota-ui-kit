f399ec785313dadd0057bd12ec412c46d1a85a11
Begoña Alvarez <balvarez@boxfish.studio>
2024-05-13 17:20:05 +0200

    refactor: improve code quality

--- src/lib/stores/isMobile.store.ts
+++ src/lib/stores/isMobile.store.ts
@@ -1,22 +1,21 @@
-import { writable } from 'svelte/store'
 import { BREAKPOINTS } from '$lib/constants'
-
-// Function to determine if the screen is mobile
-function _isMobile() {
-    // Check if window is defined (runs only in browser environment)
-    if (typeof window !== 'undefined') {
-        return window.innerWidth < BREAKPOINTS.sm
-    }
-    // Return false if window is not defined (for server-side rendering)
-    return false
-}
+import { writable, type Writable } from 'svelte/store'
 
 // Create a writable store with an initial value based on the current screen width
-export const isMobile = writable(_isMobile())
+export const isMobile: Writable<boolean> = writable(_isMobile())
 
 // Update the store value when the screen is resized
 if (typeof window !== 'undefined') {
     window.addEventListener('resize', () => {
         isMobile.set(_isMobile())
     })
+}
+
+function _isMobile(): boolean {
+    // Check if window is defined (runs only in browser environment)
+    if (typeof window !== 'undefined') {
+        return window.innerWidth < BREAKPOINTS.sm
+    }
+    // Return false if window is not defined (for server-side rendering)
+    return false
 }
